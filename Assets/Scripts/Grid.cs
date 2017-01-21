4823e95c779d200e125ce29ad0ca065805d1f1fa
Dziek Dyes-Bolt <hackingnasatips@gmail.com>
2017-01-21 18:52:56 +0000

    Wave Detector
    Detects wave progression
    Displays wave current column
    Scores for each column 'done'

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using System.Linq;
 
 public class GridInfo {
 	public Vector2 pos;
 	public bool full;
+	public CrowdMemberController crowdMember;
 	
 	public GridInfo (Vector2 p) {
 		
@@ -14,18 +14,51 @@ public class GridInfo {
 	}
 }
 
+public class Seat {
+	
+	public bool full;
+	public CrowdMemberController crowdMember;
+	
+	public Seat () {
+		full = false;
+	}
+}
+
 public class Grid {
+	
+	int currentColumn;
+	
+	int rowCount = 4;
+	Seat[,] seatColumns;
+	
+	public GameObject currentColumnPin;
 	
 	public List<GridInfo> grid = new List<GridInfo>();
 	
+	// public Vector2 scale;
+	// public Vector2 startPos;
+	private float[] columnPositions;
+	
+	private int score;
+	
 	// both starting values have to be lower than ending
-	// public Grid (int columns, int rows, Vector2 startingPos, Vector2 endingPos) {
 	public Grid (int crowdSize, Vector2 startingPos, Vector2 endingPos) {
 		
-		int rows = 4;
+		// startPos = startingPos;
+		
+		int rows = rowCount;
 		int columns = crowdSize/rows;
 		
-		int totalPeople = columns * rows;
+		columnPositions = new float[columns];
+		
+		seatColumns = new Seat[columns,rows];
+		for(int i = 0; i < columns; i++) 
+		{
+			for(int j = 0; j < rows; j++) 
+			{
+				seatColumns[i,j] = new Seat();
+			}
+		}
 		
 		Vector2 scale = new Vector2();
 		scale.x = Mathf.Abs(startingPos.x - endingPos.x) / (columns - 1);
@@ -33,9 +66,12 @@ public class Grid {
 		
 		for (int c = 0; c < columns; c++)
 		{
+			float xPos = startingPos.x + (c * scale.x);
+			columnPositions[c] = xPos;
+		
 			for (int r = 0; r < rows; r++)
 			{
-				float xPos = startingPos.x + (c * scale.x);
+				// float xPos = startingPos.x + (c * scale.x);
 				float yPos = startingPos.y + (r * scale.y);
 				
 				Vector2 pos = new Vector2(xPos, yPos);
@@ -43,75 +79,101 @@ public class Grid {
 				grid.Add(new GridInfo(pos));
 			}
 		}
-		
 	}
 	
-	public Vector2 GetEmptySeat () {
-		
-		// Vector2 pos = Vector2.zero;
-		
-		// GridInfo info
-		// .Where(n => n.full == false)
-		// .Select(n => n)
+	public Vector2 FillEmptySeat (CrowdMemberController currentCrowdMember) {
 		
 		for (int i = 0; i < grid.Count; i++)
 		{
 			if (grid[i].full == false)
 			{
-				// info = grid[i];
-				// break;
-				
 				grid[i].full = true;
+				grid[i].crowdMember = currentCrowdMember;
+				
+				FillSeat(i, currentCrowdMember);
 				return grid[i].pos;
 			}
 		}
 		
-		// info.full = true;
-		// return info.pos;
-		
 		return Vector2.zero;
 	}
 	
-	// public void EmptySeat () {
+	void FillSeat (int i, CrowdMemberController currentCrowdMember) {
+		int c = (int)Mathf.Floor(i / 4);
+		int r = i % 4;
 		
-	// }
+		seatColumns[c,r].full = true;
+		seatColumns[c,r].crowdMember = currentCrowdMember;
+	}
+	
+	public void LightUpColumn (int c) {
+		
+		if (c >= seatColumns.GetLength(0) - 1)
+		{
+			c = c % seatColumns.GetLength(0);
+		}
+	
+		for (int i = 0; i < rowCount; i++)
+		{
+			if (seatColumns[c,i].full == true)
+			{
+				seatColumns[c,i].crowdMember.gameObject.GetComponent<SpriteRenderer>().flipY = true;
+			}
+		}
+		
+		if (c > 0)
+		{
+			LightDownColumn(c-1);
+		}else{
+			LightDownColumn(seatColumns.GetLength(0)-1);
+		}
+	}
+	
+	public void LightDownColumn (int c) {
+		for (int i = 0; i < rowCount; i++)
+		{
+			if (seatColumns[c,i].full == true)
+			{
+				seatColumns[c,i].crowdMember.gameObject.GetComponent<SpriteRenderer>().flipY = false;
+			}
+		}
+	}
+	
+	public void CheckWave () {
+		
+		currentColumnPin.transform.position = new Vector2(columnPositions[currentColumn], currentColumnPin.transform.position.y);
+		
+		int c = currentColumn;
+		bool done = true;
+		
+		for (int i = 0; i < rowCount; i++)
+		{
+			if (seatColumns[c,i].full == true)
+			{
+				if (seatColumns[c,i].crowdMember.AreHandsUp() == false)
+				{
+					done = false;
+				}
+			}
+		}
+		
+		// MEGA DEBUG
+		// if (Input.GetKeyDown("Alpha"+currentColumn))
+		if (Input.GetKeyDown("m"))
+		{
+			done = true;
+		}
+		
+		if (done == true)
+		{
+			Debug.Log("DONE");
+			currentColumn++;
+			if (currentColumn == seatColumns.GetLength(0))
+			{
+				currentColumn = 0;
+			}
+			
+			score++;
+		}
+	}
 }
-	
-	// Use this for initialization
-	// void Start () {
-		// griddo = new List<Vector2>();
-		
-		// for
-		
-		
-		
-		// for (int i = 0; i < totalPeople; i++)
-		// {
-			// var newCrowdMember = GameObject.Instantiate(crowdMemberPrefab) as GameObject;
-
-			// // newCrowdMember.transform.position = griddo[i] + new Vector2(startingX, 0);
-			// newCrowdMember.transform.position = griddo[i];
-			// // grid[i].full = true;
-		// }
-	// }
-	
-	// public void MakeGrid (int c, int r, Vector2 s, Vector2 e) {
-		
-		// Vector2 scale = new Vector2();
-		// scale.x = Mathf.Abs(s.x - e.x) / (c - 1);
-		// scale.y = Mathf.Abs(s.y - e.y) / (r - 1);
-		
-		// Debug.Log(scale.y);
-		
-		// for (int i = 0; i < c; i++)
-		// {
-			// for (int j = 0; j < r; j++)
-			// {
-				// float xPos = s.x + (i * scale.x);
-				// float yPos = s.y + (j * scale.y);
-				
-				// griddo.Add(new Vector2(xPos, yPos));
-			// }
-		// }
-	// }
-// }
