2d5254e251c0b014e7a2fcbfe2b65a5a6d7b2895
rachelnertia <nershly@gmail.com>
2017-01-21 12:18:03 +0000

    Add things

--- Assets/Scripts/CrowdMemberController.cs
+++ Assets/Scripts/CrowdMemberController.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class CrowdMemberController : MonoBehaviour {
 
+    private bool handsUp;
+
+    public KeyCode keyCode;
+
+    public Sprite[] frames = new Sprite[2];
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKey(keyCode)) {
+            if (!handsUp) {
+                // Go to hands up frame.
+                gameObject.GetComponent<SpriteRenderer>().sprite = frames[1];
+
+                handsUp = true;
+            }
+        } else {
+            if (handsUp) {
+                // Go to hands down frame.
+                gameObject.GetComponent<SpriteRenderer>().sprite = frames[0]; 
+
+                handsUp = false;
+            }
+        }
 	}
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Player {
+    public List<GameObject> crowdMembers = new List<GameObject>();
+
+    public List<KeyCode> keys = new List<KeyCode>();
+
+    public HashSet<KeyCode> keysThatHaveBeenClaimed = new HashSet<KeyCode>();
+
+    public Player() {
+        keys = new List<KeyCode>();
+        keys.Add(KeyCode.Joystick1Button0);
+        keys.Add(KeyCode.Joystick1Button1);
+        keys.Add(KeyCode.Joystick1Button2);
+        keys.Add(KeyCode.Joystick1Button3);
+        keys.Add(KeyCode.Joystick1Button4);
+        keys.Add(KeyCode.Joystick1Button5);
+        keys.Add(KeyCode.Joystick1Button6);
+        keys.Add(KeyCode.Joystick1Button7);
+        keys.Add(KeyCode.Joystick1Button8);
+        keys.Add(KeyCode.Joystick1Button9);
+        keys.Add(KeyCode.Joystick1Button10);
+    }
+}
+
 public class GameController : MonoBehaviour {
 
     public GameObject crowdMemberPrefab;
@@ -15,28 +38,47 @@ public class GameController : MonoBehaviour {
 
     private GameState gameState = GameState.None;
 
-    public void CreateCrowdMember(int team) {
-        var newCrowdMember = GameObject.Instantiate(crowdMemberPrefab);
+    
+
+    public Player[] players = new Player[2];
+
+    public void CreateCrowdMember(Player player) {
+        var newCrowdMember = GameObject.Instantiate(crowdMemberPrefab) as GameObject;
+
+        newCrowdMember.transform.position = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+
         var newCrowdMemberController = newCrowdMember.GetComponent<CrowdMemberController>();
-        // TODO: Map the crowd member to a button on the correct player's controller.
-        // TODO: Position the crowd member on the correct player's side of the screen.
+        
+        KeyCode kc = player.keys[Random.Range(0, player.keys.Count)];
+
+        newCrowdMemberController.keyCode = kc;
+
+        player.crowdMembers.Add(newCrowdMember);
     }
 
     public void StartGame() {
-        // Do cleanup, if necessary.
-
         gameState = GameState.None;
+        
+        players[0] = new Player();
+        CreateCrowdMember(players[0]);
+        CreateCrowdMember(players[0]);
+        CreateCrowdMember(players[0]);
+        CreateCrowdMember(players[0]);
+        CreateCrowdMember(players[0]);
+        CreateCrowdMember(players[0]);
+        CreateCrowdMember(players[0]);
+        CreateCrowdMember(players[0]);
+        CreateCrowdMember(players[0]);
+        CreateCrowdMember(players[0]);
 
-        // TODO: Set up players
-
-        // TODO: Generate crowd members
-
-        // TODO: Go into 'start of game' mode, in which players can warm up before starting.
+        gameState = GameState.Warmup;
     }
 
     // Use this for initialization
 	void Start () {
-		
+        players = new Player[2];
+
+        StartGame();
 	}
 	
 	// Update is called once per frame
