9b8f1ccb4be3754f49de2181c433e9c04178ae1f
rachelnertia <nershly@gmail.com>
2017-01-21 10:35:31 +0000

    More setup

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,19 +6,29 @@ public class GameController : MonoBehaviour {
 
     public GameObject crowdMemberPrefab;
 
-    private enum GameStates {
+    private enum GameState {
         None,
         Warmup,
         InProgress,
         End
     }
 
+    private GameState gameState = GameState.None;
+
     public void CreateCrowdMember(int team) {
         var newCrowdMember = GameObject.Instantiate(crowdMemberPrefab);
-        var newCrowdMemberController = crowdMemberPrefab.GetComponent<CrowdMemberController>();
+        var newCrowdMemberController = newCrowdMember.GetComponent<CrowdMemberController>();
+        // TODO: Map the crowd member to a button on the correct player's controller.
+        // TODO: Position the crowd member on the correct player's side of the screen.
     }
 
     public void StartGame() {
+        // Do cleanup, if necessary.
+
+        gameState = GameState.None;
+
+        // TODO: Set up players
+
         // TODO: Generate crowd members
 
         // TODO: Go into 'start of game' mode, in which players can warm up before starting.
@@ -31,6 +41,6 @@ public class GameController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+    }
 }
