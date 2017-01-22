8eeeeb667e4908b2af6a2cd09888e19857140589
rachelnertia <nershly@gmail.com>
2017-01-22 14:04:37 +0000

    Add prompt for player to press A to title screen.

--- Assets/Scripts/TitleScreenController.cs
+++ Assets/Scripts/TitleScreenController.cs
@@ -12,7 +12,7 @@ public class TitleScreenController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0) || Input.GetKeyDown(KeyCode.Joystick2Button0)) {
             SceneManager.LoadScene("DefaultScene");
         }
     }
